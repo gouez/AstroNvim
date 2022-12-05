9dbd19d8c94beb91bbdc283e65ab2c64ecff034d
gouez <gqsforme@gmail.com>
2022-12-05 22:24:16 +0800

    init

---
+++ lua/user/plugins/bufferline.lua
@@ -0,0 +1,8 @@
+return {
+  options = {
+    offsets = {
+      -- options function , text_" "h always_show_bufferline = false
+      { filetype = "neo-tree", text = "File Explorer", text_align = "center" },
+    },
+  }
+}
